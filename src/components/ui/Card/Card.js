d51f3dd0c41044854b5542692faaef25a7fca521
Rabii Luena <rbluena@gmail.com>
2020-05-18 07:25:31 +0300

    feat: adding score card

--- src/components/ui/Card/Card.js
+++ src/components/ui/Card/Card.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 import Text from "../Text";
 import Avatar from "../Avatar";
 import Button from "../Button";
@@ -18,29 +19,69 @@ const Card = ({ heading, children }) => {
   );
 };
 
-Card.Poll = ({ onClick }) => {
+Card.defaultProps = {
+  heading: "",
+};
+
+Card.propTypes = {
+  heading: PropTypes.string,
+  children: PropTypes.node.isRequired,
+};
+
+Card.Poll = ({ onClick, heading }) => {
   return (
-    <Card>
+    <Card heading={heading}>
       <div className="card__left-content">
         <Avatar src="" alt="" />
       </div>
       <div className="card__right-content">
         <Text type="title">Would you rather</Text>
-      </div>
-      <div className="card__section">
-        <Text type="paragraph">...be...</Text>
-        <div className="card__buttons">
-          <Button primary wide onClick={onClick}>
-            Button
-          </Button>
+        <div className="card__section">
+          <Text type="paragraph">...be...</Text>
+          <div className="card__buttons">
+            <Button primary wide onClick={onClick}>
+              Button
+            </Button>
+          </div>
         </div>
       </div>
     </Card>
   );
 };
 
-Card.Score = () => {
-  return <h2>Something else</h2>;
+Card.Poll.propTypes = {
+  onClick: PropTypes.func.isRequired,
+};
+
+Card.Score = ({ imgSrc, heading }) => {
+  return (
+    <Card heading={heading}>
+      <div className="card__left-content">
+        <Avatar src={imgSrc} alt={heading} />
+      </div>
+      <div className="card__right-content">
+        <div className="card__question-box">
+          <h3 className="heading-3">
+            Created questions <span className="card__title-number">10</span>{" "}
+          </h3>
+          <hr className="devider devider__horizontal" />
+          <h3 className="heading-3">
+            Created answers <span className="card__title-number">10</span>{" "}
+          </h3>
+        </div>
+        <div className="score-box" value={5} text="Some text" />
+      </div>
+    </Card>
+  );
+};
+
+Card.Score.defaultProps = {
+  imgSrc: "",
+};
+
+Card.Score.propTypes = {
+  heading: PropTypes.string.isRequired,
+  imgSrc: PropTypes.string,
 };
 
 export default Card;
