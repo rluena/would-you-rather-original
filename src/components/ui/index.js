d590121bf1676f2446ae5586a3be6c1ce9358351
Rabii Luena <rbluena@gmail.com>
2020-05-18 08:10:51 +0300

    fix: text component is not accessible

--- src/components/ui/index.js
+++ src/components/ui/index.js
@@ -1,5 +1,6 @@
 import Avatar from "./Avatar";
 import Button from "./Button";
 import Card from "./Card";
+import Text from "./Text";
 
-export { Avatar, Button, Card };
+export { Avatar, Button, Card, Text };
